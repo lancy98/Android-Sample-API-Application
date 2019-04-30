163c419587e9ab505005d26533d73d98409873a1
Lancy Norbert Fernandes <>
2019-04-30 20:30:41 +1200

    - Implementation of currency exchange rates API

--- app/src/main/java/com/example/apiapplication/MainActivity.java
+++ app/src/main/java/com/example/apiapplication/MainActivity.java
@@ -2,12 +2,52 @@ package com.example.apiapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RequestQueue requestQueue;
+    public final String TAG = "API-APP-LOG";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        requestQueue = Volley.newRequestQueue(this);
+        String url = "https://api.exchangeratesapi.io/latest";
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET,
+                url,
+                null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        Log.d(TAG, "success: " + response.toString());
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.d(TAG, "response error");
+                    }
+                }
+        );
+
+        requestQueue.add(jsonObjectRequest);
     }
 }
